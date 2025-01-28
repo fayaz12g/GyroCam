b6904e3d82d4d6f97197dad82f3163dde39c46cc
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2025-01-28 16:38:51 -0500

    button color and shape

--- GyroCam/RecordingButton.swift
+++ GyroCam/RecordingButton.swift
@@ -18,9 +18,9 @@ struct RecordingButton: View {
                     .frame(width: 70, height: 70)
                 
                 // Animated red shape
-                RoundedRectangle(cornerRadius: isRecording ? 4 : 25)
-                    .fill(Color.red)
-                    .frame(width: isRecording ? 34 : 56, height: isRecording ? 34 : 56)
+                RoundedRectangle(cornerRadius: isRecording ? 7 : 60)
+                    .fill(Color.init(hex: "ff000d"))
+                    .frame(width: isRecording ? 28 : 60, height: isRecording ? 28 : 60)
                     .animation(.spring(response: 0.3, dampingFraction: 0.6), value: isRecording)
                 
                 // Scaling pulse effect
@@ -48,5 +48,32 @@ struct RecordingButtonStyle: ButtonStyle {
         configuration.label
             .scaleEffect(configuration.isPressed ? 0.92 : 1)
             .animation(.interactiveSpring(response: 0.3, dampingFraction: 0.6), value: configuration.isPressed)
+    }
+}
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
     }
 }
